34c0793bbcb725d6c70810a5b8c2d08ce903492b
James McMahon <jmcmahon@skillz.com>
2014-08-12 18:09:00 -0400

    Clean up and comments

--- Assets/SKZ/SKZInitTestApp.cs
+++ Assets/SKZ/SKZInitTestApp.cs
@@ -1,24 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * This scene controls Skillz initialization and has a button to launch the Skillz interface.
+ */
 public class SKZInitTestApp : MonoBehaviour {
-
-	void OnGUI() {
-		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 6, 200, Screen.height / 4), "Launch Skillz")) {
-			Skillz.launchSkillz (Skillz.SkillzOrientation.SkillzLandscape);
-		}
-	}
-
+		
 	// Use this for initialization
 	void Start () {
-		// 213 is the Game ID
-		// SkillzSandbox is the Skillz server to connect to
-		// (SkillzSandbox for development/testing).
+		// General Skillz initialization
+		// Id is given to us by the devportal
 		Skillz.skillzInitForGameIdAndEnvironment("110", Skillz.SkillzEnvironment.SkillzSandbox);
 	}
 
-	// Update is called once per frame
-	void Update () {
-
+	void OnGUI() {
+		// Launch Skillz on button press
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 6, 200, Screen.height / 4), "Launch Skillz")) {
+			Skillz.launchSkillz (Skillz.SkillzOrientation.SkillzLandscape);
+		}
 	}
 }
--- Assets/SKZ/SKZTestApp.cs
+++ Assets/SKZ/SKZTestApp.cs
@@ -1,41 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * This scene is fired when the user clicks play in the Skillz interface. 
+ * It has a few buttons for the reporting mock scores to Skillz.
+ * 
+ * Usually these scores would be generated through normal gameplay, 
+ * but this example sets these scores with fake values.
+ */
 public class SKZTestApp : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
+
 	// GUI callback
 	void OnGUI() {
-		//		if (GUI.Button (new Rect(Screen.width / 2 - 75,50,250,100), "Score HIGH")) {
-		//			Skillz.displayTournamentResultsWithScore(99999);
-		//		}
-		//
-		//		if (GUI.Button (new Rect(Screen.width / 2 - 75,175,250,100), "Score LOW")) {
-		//			int score = Random.Range(0, 10000);
-		//			Skillz.displayTournamentResultsWithScore(score);
-		//		}
-		
-		if (GUI.Button (new Rect(Screen.width / 2 - 75,300,250,100), "ABORT")) {
-			Skillz.notifyPlayerAbortWithCompletion();
+		// Basic implementation
+		if (GUI.Button (new Rect(0, Screen.height / 4, 250, 100), "Score HIGH")) {
+			// Report a score to Skillz
+			Skillz.displayTournamentResultsWithScore(99999);
+		}
+
+		if (GUI.Button (new Rect(275, Screen.height / 4, 250, 100), "Score LOW")) {
+			int score = Random.Range(0, 10000);
+			Skillz.displayTournamentResultsWithScore(score);
 		}
 		
-		if (GUI.Button (new Rect(Screen.width / 2 - 75, 425, 250, 100), "SCORE HIGH TURN BASED")) {
+		if (GUI.Button (new Rect(550, Screen.height / 4, 250, 100), "ABORT")) {
+			// Aborting is a way for a user to back out of a game
+			Skillz.notifyPlayerAbortWithCompletion();
+		}
+
+		// Sample turn based implementation (most games do not need to worry about this) 
+		if (GUI.Button (new Rect(825, Screen.height / 4, 250, 100), "SCORE HIGH TURN BASED")) {
 			Skillz.completeTurnWithGameData("GAMEDATASON", "99999", 99999, 0, Skillz.SkillzTurnBasedRoundOutcome.SkillzRoundNoOutcome, Skillz.SkillzTurnBasedMatchOutcome.SkillzMatchNoOutcome);
 		}
 		
-		if (GUI.Button (new Rect(Screen.width / 2 - 75, 550, 250, 100), "SCORE LOW TURN BASED")) {
+		if (GUI.Button (new Rect(0, Screen.height / 2, 250, 100), "SCORE LOW TURN BASED")) {
 			Skillz.completeTurnWithGameData("GAMEDATASON", "0", 0, 0, Skillz.SkillzTurnBasedRoundOutcome.SkillzRoundNoOutcome, Skillz.SkillzTurnBasedMatchOutcome.SkillzMatchNoOutcome);
 		}
 		
-		if (GUI.Button (new Rect(Screen.width / 2 - 75, 675, 250, 100), "FINISH REVIEWING GAME STATE")) {
+		if (GUI.Button (new Rect(275, Screen.height / 2, 250, 100), "FINISH REVIEWING GAME STATE")) {
 			Skillz.finishReviewingCurrentGameState();
 		}
 	}
